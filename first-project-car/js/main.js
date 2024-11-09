be0ed9a0af128c5ccfd69f44f3517dfc0f149071
SasnykVova <sasnyk2000@ukr.net>
2024-11-09 07:52:10 +0200

    add some func

--- first-project-car/js/main.js
+++ first-project-car/js/main.js
@@ -12,4 +12,35 @@ $(document).ready(function() {
         });
     })
 
+    let modelSpecs,
+        modelPrice,
+        modelSpecsHolder,
+        modelPriceHolder;
+    
+    modelSpecsHolder = $('#model-specs');
+    modelPriceHolder = $('#model-price');
+
+    modelPrice = 0;
+    modelSpecs = '';
+
+    function calculatePrice() {
+        modelPrice = parseInt($('input[name=engine]:checked', '#autoForm').val());
+        modelPrice += parseInt($('input[name=transmission]:checked', '#autoForm').val());
+        modelPrice += parseInt($('input[name=package]:checked', '#autoForm').val());
+        const formattedNumber = modelPrice.toLocaleString('en').replace(/,/g, ' ');
+        modelPriceHolder.text(formattedNumber + ' грн');
+    }
+    calculatePrice();
+    $('#autoForm input').on('change', function() {
+        calculatePrice();   
+        getSpecs()
+    })
+    
+    function getSpecs() {
+        modelSpecs = $('input[name=engine]:checked + label', '#autoForm').text();
+        modelSpecs = modelSpecs + ', ' + $('input[name=transmission]:checked + label', '#autoForm').text();
+        modelSpecs = modelSpecs + ', ' + $('input[name=package]:checked + label', '#autoForm').text();
+        modelSpecsHolder.text(modelSpecs);
+    }
+    getSpecs()
 })
